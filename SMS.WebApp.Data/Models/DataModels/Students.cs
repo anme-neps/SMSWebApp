f3685d7d204f93276aae5cce37e7a445a2fc9144
Arjun Neupane <arjun.neupane@javra.com>
2023-07-21 07:48:18 +0545

    add fk to image table exmple

--- SMS.WebApp.Data/Models/DataModels/Students.cs
+++ SMS.WebApp.Data/Models/DataModels/Students.cs
@@ -2,6 +2,7 @@
 using SMS.WebApp.Data.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,14 @@ namespace SMS.WebApp.Data.Models.DataModels
         public GenderEnum Gender { get; set; }
         public string GradeLevel { get; set; }
         public string PhoneNumber { get; set; }
+        // Example Only
+        public byte[] ImageData { get; set; }
+        // foreign key -> Image table One to Many 
+        public virtual IEnumerable<Image> Images { get; set; }
+        // one to one
+        [ForeignKey(nameof(Image))]
+        public int ImageId { get; set; }
+        public virtual Image Image { get; set; }
+
     }
 }
